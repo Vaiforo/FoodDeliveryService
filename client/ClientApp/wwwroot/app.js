366a0dd8587a74155b226a9cd8a800b266b93460
vaiforic-win-pc <abobkof@mail.ru>
2026-03-12 18:20:02 +0600

    feat: add websocket client logic in app.js

---
+++ client/ClientApp/wwwroot/app.js
@@ -0,0 +1,30 @@
+const statusEl = document.getElementById("status");
+const msgEl = document.getElementById("msg");
+const btn = document.getElementById("btn");
+
+const ws = new WebSocket("ws://food-ws.local/ws");
+
+ws.addEventListener("open", () => {
+    statusEl.textContent = "WebSocket: соединение успешно установлено";
+    btn.disabled = false;
+});
+
+ws.addEventListener("message", (event) => {
+    msgEl.textContent = "";
+    msgEl.textContent = event.data;
+});
+
+ws.addEventListener("close", () => {
+    statusEl.textContent = "WebSocketL соедиенение потеряно";
+    btn.disabled = true;
+});
+
+ws.addEventListener("error", () => {
+    statusEl.textContent = "WebSocket: ошибка соединения";
+    btn.disabled = true;
+});
+
+btn.addEventListener("click", () => {
+    const unique = `support:${crypto.randomUUID()}`;
+    ws.send(unique);
+});
