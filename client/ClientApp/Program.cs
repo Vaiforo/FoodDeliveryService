faa9b54e75c0f7af0215f3c0a85da6527e9c2255
vaiforic-win-pc <abobkof@mail.ru>
2026-03-12 18:09:34 +0600

    feat: implement basic client logic

--- client/ClientApp/Program.cs
+++ client/ClientApp/Program.cs
@@ -1,6 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.MapGet("/", () =>
+{
+    var htmlResponse = """
+        <!doctype html>
+        <html lang="ru">
+        <head>
+        <meta charset="utf-8">
+        <title>Client</title>
+        </head>
+        <body>
+        <h1>Food Delivery Service</h1>
+        <h2 id="status"></h2>
+        <p id="msg"></p>
+        <button id="btn" disabled>Хороший сервис</button>
+
+        <script src="/app.js"></script>
+        </body>
+        </html>
+    """;
+
+    return Results.Text(htmlResponse, "text/html; charset=utf-8");
+});
 
 app.Run();
